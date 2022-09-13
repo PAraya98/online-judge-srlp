04ba444bc8be4ce625518eb87ce5d4802a48cffd
root <root@dmoj>
2022-09-13 15:39:55 -0700

    first_commit

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '5*9f5q57mqmlz2#f$x1h76&jxy#yortjl1v+l*6hd18$d*yx#0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.99','186.156.92.127', '192.168.0.100','localhost', '127.0.0.1', '0.0.0.0']
 
 SITE_ID = 1
 SITE_NAME = 'DMOJ'
