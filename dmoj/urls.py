39d7529311fbc4f90114f6669e6ca9a83f242f8b
PAraya98 <pedroaraya3098@gmail.com>
2022-09-15 22:27:56 -0300

    Prueba de primera API Register

--- dmoj/urls.py
+++ dmoj/urls.py
@@ -97,7 +97,8 @@ def paged_list_view(view, name):
 
 urlpatterns = [
      path('api_srlp/auth/', include([
-        path('register', api.APIUserAuth.register),
+        path('register', api.register),
+        path('login', api.get_tokens_for_user),
     ])),
     path('', blog.PostList.as_view(template_name='home.html', title=_('Home')), kwargs={'page': 1}, name='home'),
     path('500/', exception),
