cc53d3740677923187aa6f768e156ddc47f9632a
PAraya98 <pedroaraya3098@gmail.com>
2022-09-19 20:50:35 -0300

    refactoring y definición de interfaces para obtener problemas

--- dmoj/urls.py
+++ dmoj/urls.py
@@ -94,8 +94,8 @@ def paged_list_view(view, name):
 
 
 urlpatterns = [
-    path('srlp_api/', include(srlp_patterns)),
     path('', blog.PostList.as_view(template_name='home.html', title=_('Home')), kwargs={'page': 1}, name='home'),
+    path('srlp_api/', include(srlp_patterns)),
     path('500/', exception),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
