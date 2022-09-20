0fdb6687ca60b76c36c89bf9250835f06da42ed7
PAraya98 <pedroaraya3098@gmail.com>
2022-09-19 23:43:47 -0300

    Pruebas de API Rest

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = '5*9f5q57mqmlz2#f$x1h76&jxy#yortjl1v+l*6hd18$d*yx#0'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ORIGIN_ALLOW_ALL = True
 #['192.168.0.99','186.156.92.127', '192.168.0.100','localhost', '127.0.0.1', '0.0.0.0']
 #ALLOWED_HOSTS = ['192.168.0.99','186.156.92.127', '192.168.0.100','localhost', '127.0.0.1', '0.0.0.0']
-#CORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = ['*']
 #    'http://152.173.159.165',
 #]
 
