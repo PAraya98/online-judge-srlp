baedd080cc91ee89f559cb3f2495f55101a70871
PAraya98 <pedroaraya3098@gmail.com>
2022-09-19 21:00:04 -0300

    refactoring y definición de interfaces para obtener problemas

--- dmoj/urls_api_srlp.py
+++ dmoj/urls_api_srlp.py
@@ -1,12 +1,11 @@
 from django.urls import include, path, re_path, reverse
-
 from judge.views.api import srlp_auth
 
 srlp_auth_patterns = [
-    [   path('register', srlp_auth.register),
-        path('login', srlp_auth.get_tokens_for_user),
-        path('validation', srlp_auth.HelloView.post),
-    ]   
+    path('register', srlp_auth.register),
+    path('login', srlp_auth.get_tokens_for_user),
+    path('validation', srlp_auth.HelloView.post),
+       
 ]
 
 srlp_patterns= [
