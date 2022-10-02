a9b5ac7d6b79d85a31e9cb6a6ca3ffdf3bc5d7bc
PAraya98 <pedroaraya3098@gmail.com>
2022-10-02 16:25:34 -0300

    arreglando rutas de búsqueda

--- judge/views/api/srlp/srlp_problem.py
+++ judge/views/api/srlp/srlp_problem.py
@@ -22,7 +22,7 @@ def get_problem_list(request):
         query = ' '.join(request.GET.getlist()).strip()
         print(query)
         if query:
-            queryset = queryset.search(query)
+            queryset = queryset.search(['code = aplusb'])
     queryset = queryset.values_list('code', 'points', 'partial', 'name', 'group__full_name')
     return Response({code: {
         'points': points,
