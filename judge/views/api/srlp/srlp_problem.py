ada16fa3338c765394bb1282e4fc0df911d4d1c1
PAraya98 <pedroaraya3098@gmail.com>
2022-10-02 12:12:40 -0300

    arreglando rutas de búsqueda

--- judge/views/api/srlp/srlp_problem.py
+++ judge/views/api/srlp/srlp_problem.py
@@ -16,6 +16,7 @@ from judge.views.api.srlp.utils_srlp_api import get_jwt_user
 @api_view(['GET'])
 def get_problem_list(request):
     queryset = Problem.get_public_problems()
+    print(request.GET)
     if settings.ENABLE_FTS and 'search' in request.GET:
         query = ' '.join(request.GET.getlist('search')).strip()
         print(query)
