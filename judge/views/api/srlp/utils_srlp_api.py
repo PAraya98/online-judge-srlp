fe729563762552c41ad1486c365f6b3df985da64
PAraya98 <pedroaraya3098@gmail.com>
2022-10-05 20:28:34 -0300

    nuevo registro

--- judge/views/api/srlp/utils_srlp_api.py
+++ judge/views/api/srlp/utils_srlp_api.py
@@ -24,9 +24,8 @@ class CustomPagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 50
-    def __init__(self):
+    def __init__(self, **kwargs):
         pass
-
     def custom_get_paginated_response(self, data):
          return Response(OrderedDict([
             ('count', self.page.paginator.count),
