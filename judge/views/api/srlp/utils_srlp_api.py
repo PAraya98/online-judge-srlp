32bac36eb94a5dacaa2f6a901a7fc6276efe6dd4
PAraya98 <pedroaraya3098@gmail.com>
2022-10-05 19:58:22 -0300

    nuevo registro

--- judge/views/api/srlp/utils_srlp_api.py
+++ judge/views/api/srlp/utils_srlp_api.py
@@ -27,7 +27,7 @@ class CustomPagination(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
         #response = super(CustomPagination, self).get_paginated_response(data)
         data['total_pages'] = self.page.paginator.num_pages
-        return Response({data}, status=200)
+        return Response(data, status=200)
 
 ######################################################
 #PERMISOS DE USUARIO
