db603d80ea285dec33b2d0cffe4d16a0558765c1
PAraya98 <pedroaraya3098@gmail.com>
2022-10-05 20:16:09 -0300

    nuevo registro

--- judge/views/api/srlp/srlp_admin.py
+++ judge/views/api/srlp/srlp_admin.py
@@ -37,7 +37,7 @@ def get_users_info(request):
                             'rank': rank,
                         })        
         data = {'usuarios': array}
-        return paginator.custom_get_paginated_response(array)
+        return paginator.get_paginated_response(array)
     else:
         return Response({})
         
