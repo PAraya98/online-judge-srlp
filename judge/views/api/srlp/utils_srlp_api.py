857b188434fd7e81ac9411eac84988f83dc673bc
PAraya98 <pedroaraya3098@gmail.com>
2022-10-05 20:44:52 -0300

    nuevo registro

--- judge/views/api/srlp/utils_srlp_api.py
+++ judge/views/api/srlp/utils_srlp_api.py
@@ -30,8 +30,7 @@ class CustomPagination(pagination.PageNumberPagination):
             ('count', self.page.paginator.count),
             ('next', self.get_next_link()),
             ('previous', self.get_previous_link()),
-            ('results', data)
-            ('xD', "xd"),
+            ('xD', data)
         ]))
 
     def custom_get_paginated_response(self, data):
