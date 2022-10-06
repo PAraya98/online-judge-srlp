67a75fb8a5d592756e00ce3bffe162c99e59f273
PAraya98 <pedroaraya3098@gmail.com>
2022-10-05 22:00:39 -0300

    nuevo registro

--- judge/views/api/srlp/utils_srlp_api.py
+++ judge/views/api/srlp/utils_srlp_api.py
@@ -1,4 +1,4 @@
-from requests import Response
+from rest_framework.response import Response
 from collections import OrderedDict, namedtuple
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import permissions
