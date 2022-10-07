57f68a2942571bd2065dadeeec0813ba673d7a53
PAraya98 <pedroaraya3098@gmail.com>
2022-10-06 23:29:44 -0300

    Registro y Modificación de usuario

--- judge/views/api/srlp/srlp_admin.py
+++ judge/views/api/srlp/srlp_admin.py
@@ -27,16 +27,22 @@ def get_users_info(request):
             user__last_name__icontains=request.GET.get('apellidos'),
             display_rank=request.GET.get('rank')
         )
-    if(request.GET.get('order_by') is not None): queryset.order_by(request.GET.get('order_by'))
     
-    queryset = queryset.values_list('user__username', 'user__first_name', 'user__last_name', 
-    'user__email', 'display_rank', 'last_access')
+    if(request.GET.get('order_by') is not None): queryset = queryset.order_by(request.GET.get('order_by'))
+    queryset = queryset.values_list(
+        'last_access', 
+        username=F('user__username'), 
+        nombre=F('user__first_name'),
+        apellidos=F('user__last_name'),
+        rank=F('display_rank'),
+        email=F('user__email')
+    )
     
     if len(queryset)> 0:
         paginator = CustomPagination()
         result_page = paginator.paginate_queryset(queryset, request)
         array = []
-        for username, nombre, apellidos, email, rank, last_access in result_page:
+        for email, last_access, username, nombre, apellidos, rank in result_page:
             array.append({  'username': username,
                             'email': email,
                             'Nombre':  nombre, 
