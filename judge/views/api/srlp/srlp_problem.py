651507d920a5c892a756d6a22d8115b00b7e273d
PAraya98 <pedroaraya3098@gmail.com>
2022-10-13 22:05:55 -0300

    búsqueda mejorada

--- judge/views/api/srlp/srlp_problem.py
+++ judge/views/api/srlp/srlp_problem.py
@@ -95,7 +95,7 @@ def get_problem_info(request):
         'partial': p.partial,
         'languages': list(p.allowed_languages.values_list('key', flat=True)),
         'description': p.description,        
-        'description2': markdown(p.markdown_style, p.description)
+        'description2': markdown(p.description, p.markdown_style)
     })
     
 #@action(methods=['GET'], detail=False)
