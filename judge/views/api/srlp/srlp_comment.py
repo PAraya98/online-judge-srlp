c4824abdd8ae801237c2961e7e62f1013adf1f96
PAraya98 <pedroaraya3098@gmail.com>
2022-10-18 23:51:29 -0300

    Implementación de submit

--- judge/views/api/srlp/srlp_comment.py
+++ judge/views/api/srlp/srlp_comment.py
@@ -31,6 +31,7 @@ def create_comment(request):
 def get_comments(request):
     comment_aux = Comment.objects.create(page=request.GET.getlist('page_code')[0], author_id=1)
     if(comment_aux.is_public() or comment_aux.is_accessible_by(get_jwt_user(request))):
+        comment_aux = None
         queryset = Comment.objects.filter(page=request.GET.getlist('page_code')[0]).exclude(hidden=True).values()
         if len(queryset)> 0:
             paginator = CustomPagination()
