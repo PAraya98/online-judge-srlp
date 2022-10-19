710e571fd20c498d90f547b8e94e33d59f5ed5e6
PAraya98 <pedroaraya3098@gmail.com>
2022-10-18 23:48:37 -0300

    Implementación de submit

--- judge/views/api/srlp/srlp_comment.py
+++ judge/views/api/srlp/srlp_comment.py
@@ -47,6 +47,7 @@ def get_comments(request):
                     "rght": comment.rght,
                     "tree_id": comment.tree_id,
                     "author": {
+                        "author_id": comment.author_id,
                         "username": user.username,
                         "gravatar": gravatar_username(user.username)
                     },
