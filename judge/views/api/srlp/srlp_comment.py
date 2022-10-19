48523c472dd9d51a354e3dae7198a6cc0aea8388
PAraya98 <pedroaraya3098@gmail.com>
2022-10-19 19:14:35 -0300

    Implementación de comentarios

--- judge/views/api/srlp/srlp_comment.py
+++ judge/views/api/srlp/srlp_comment.py
@@ -14,18 +14,30 @@ from judge.models.runtime import Language
 from django.contrib.auth.models import User
 from judge.views.api.srlp.srlp_utils_api import get_jwt_user, CustomPagination, isLogueado, filter_if_not_none
 from judge.jinja2.gravatar import gravatar_username
+
 @permission_classes([isLogueado])
 @api_view(['POST'])
 def create_comment(request):
     data = DefaultMunch.fromDict(json.loads(request.body))
-    user = get_jwt_user(request)
-    profile= Profile.objects.get(user=user)
-    comment = Comment.objects.create(page=data.page, author=profile, body=data.body, parent=data.parent)
-    if(comment.is_accessible_by(user)):
-        comment.save()
-        return Response({'status': True})
-    else:
-        return Response({'status': False})
+    comment_aux = get_list_or_404(Comment, page=data.page_code)[0]    
+    if(comment_aux.is_accessible_by(get_jwt_user(request))):
+
+        user = get_jwt_user(request)
+        profile= Profile.objects.get(user=user)
+
+        if not user.is_staff and not profile.has_any_solves:
+            return Response({'status': False, 'message': 'Debes resolver al menos un problema para poder comentar.'})
+        
+        if profile.mute:
+            return Response({'status': False, 'message': 'Tú cuenta ha sido silenciada por el administrador.'})
+
+        comment = Comment.objects.create(page=data.page_code, author_id=profile.id, body=data.body, parent=data.parent)
+        
+        if(comment.is_accessible_by(user)):
+            comment.save()
+            return Response({'status': True})
+        else:
+            return Response({'status': False, 'message': 'No tienes acceso a esta acción.'})
 
 @api_view(['GET'])
 def get_comments(request):
