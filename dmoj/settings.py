4175bfe8efd67ce10bf754b67256d2ddaf4a6107
PAraya98 <pedroaraya3098@gmail.com>
2022-10-18 23:18:13 -0300

    Implementación de submit

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -296,7 +296,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'judge.views.api.srlp.utils_srlp_api.CustomPagination',
+    'DEFAULT_PAGINATION_CLASS': 'judge.views.api.srlp.srlp_utils_api.CustomPagination',
     'DEFAULT_METADATA_CLASS': 'rest_framework.metadata.SimpleMetadata'
 }
 
