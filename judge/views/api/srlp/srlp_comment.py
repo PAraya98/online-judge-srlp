fd4e283eae7344a3e05492eccc249d434d009488
PAraya98 <pedroaraya3098@gmail.com>
2022-10-19 23:53:08 -0300

    Implementación de comentarios

--- judge/views/api/srlp/srlp_comment.py
+++ judge/views/api/srlp/srlp_comment.py
@@ -105,7 +105,7 @@ def recursive_comment_query(request, comments, level):
             })
            
         return {
-            paginator_comments.get_paginated_data()
+            paginator_comments.get_paginated_data({'comments': array_comments})
         }
     else: 
         return []
