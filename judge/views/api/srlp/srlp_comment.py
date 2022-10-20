2ee9624262b237c12abbf2cefd2fd10dc45a075c
PAraya98 <pedroaraya3098@gmail.com>
2022-10-20 00:52:26 -0300

    Implementación de comentarios

--- judge/views/api/srlp/srlp_comment.py
+++ judge/views/api/srlp/srlp_comment.py
@@ -69,11 +69,11 @@ def recursive_comment_query(request, comments, level):
         for comment in comments:            
             profile = Profile.objects.get(id=comment.author_id)
             user = User.objects.get(id=profile.user_id)
-            if(comment.parent != None):            
+            if(comment.parent_id != None):            
                 print("Entre!")
                 if(request.GET['response_page_size'] is not None): response_size = request.GET['response_page_size']
                 else: response_size = 4
-                comment_responses = Comment.objects.filter(page=request.GET.getlist('page_code')[0], parent=comment.id).exclude(hidden=True)[:response_size]
+                comment_responses = Comment.objects.filter(page=request.GET.getlist('page_code')[0], parent_id=comment.id).exclude(hidden=True)[:response_size]
                 array_responses = recursive_comment_query(request, comment_responses, level+1)
                 
             else:
