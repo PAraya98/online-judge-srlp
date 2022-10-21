9593a143d3ff22cab11629aba76cf391ef8dbb77
PAraya98 <pedroaraya3098@gmail.com>
2022-10-21 00:36:16 -0300

    Implementación de comentarios

--- judge/views/api/srlp/srlp_comment.py
+++ judge/views/api/srlp/srlp_comment.py
@@ -34,7 +34,7 @@ def add_comment(request):
     else:
         comment_parent = get_object_or_404(Comment, id=data.parent_id)
         print(comment_parent.level)
-        if(comment_parent.level < 3):
+        if(comment_parent.level < 2):
             comment = Comment.objects.create(page=data.page_code, author_id=profile.id, body=data.body, parent=comment_parent)
         else:
             return Response({'status': False, 'message': 'El comentario excede el número de hijos.'})
