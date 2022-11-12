3a1f5762bd04c810f8e87679e203f1dc57220123
PAraya98 <pedroaraya3098@gmail.com>
2022-11-12 19:07:09 -0300

    implementación de Wikis

--- judge/views/api/srlp/srlp_user.py
+++ judge/views/api/srlp/srlp_user.py
@@ -56,7 +56,8 @@ def get_ranking(request):
 @permission_classes([IsAuthenticated])
 def get_user_info(request):
     code = request.GET.getlist('username') 
-    username = get_jwt_user(request).username if not code and code == '' else code[0]
+    username = get_jwt_user(request).username if not code or code == '' else code[0]
+    
     profile = Profile.objects.filter(user__username=username).first()
     if not profile: return Response({'status': False, 'message': 'Error al mostrar perfil, revisa la solicitud.'})
 
