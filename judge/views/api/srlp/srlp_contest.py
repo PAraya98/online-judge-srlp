ebbeb34228a2c721292508b8ae0a370dfc037464
PAraya98 <pedroaraya3098@gmail.com>
2022-11-13 21:00:27 -0300

    implementación de Wikis

--- judge/views/api/srlp/srlp_contest.py
+++ judge/views/api/srlp/srlp_contest.py
@@ -222,6 +222,7 @@ def get_participation_info(contest_problems, participation):
     data = []
     for problem in contest_problems:
         submission_data = participation.submissions.all().filter(problem__problem=problem).first()
+        print(submission_data)
         test_cases = submission_data.submission.test_cases
         total_testcases = test_cases.count()
         correct_testcases = test_cases.filter(status='AC').count()
