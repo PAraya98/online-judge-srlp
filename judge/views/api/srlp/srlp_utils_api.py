dd99b1e8cf38e9fee248ad29fe7afcadbf01dc93
PAraya98 <pedroaraya3098@gmail.com>
2022-11-17 02:36:29 -0300

    bug fixes

--- judge/views/api/srlp/srlp_utils_api.py
+++ judge/views/api/srlp/srlp_utils_api.py
@@ -60,35 +60,50 @@ def RepresentsInt(s):
         return False
 
 
+def verify_contest(profile):
+    if profile.current_contest != None \
+     and profile.current_contest.contest.time_before_end == None:
+        profile.current_contest = None
+        profile.save()
+    
+
 ######################################################
 #PERMISOS DE USUARIO
 
 
-class isLogueado(IsAuthenticated):
-    pass
+
+
+class isLogueado(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if(bool(request.user and request.user.is_authenticated)):
+            verify_contest(request.user.profile)
+            return True
+        else: return False
 
 class IsAdministrador(permissions.BasePermission):
     def has_permission(self, request, view):
         if(bool(request.user and request.user.is_authenticated)):
+            verify_contest(request.user.profile)
             return bool(request.user.is_superuser)
         else: return False
 
 class isProfesor(permissions.BasePermission):
     def has_permission(self, request, view):
         if(bool(request.user and request.user.is_authenticated)):
+            verify_contest(request.user.profile)
             return bool(request.user.is_staff)
         else: return False
 
 class isAlumno(permissions.BasePermission):
     def has_permission(self, request, view):
         if(bool(request.user and request.user.is_authenticated)):
-            profile = get_object_or_404(Profile, user__username=request.user.username)
-            return bool(profile.display_rank == "Alumno")
+            verify_contest(request.user.profile)
+            return bool(request.user.profile.display_rank == "Alumno")
         else: return False
 
 class isVisitante(permissions.BasePermission):
     def has_permission(self, request, view):
         if(bool(request.user and request.user.is_authenticated)):
-            profile = get_object_or_404(Profile, user__username=request.user.username)
-            return bool(profile.display_rank == "Visitante")
+            return bool(request.user.profile.display_rank == "Visitante")
         else: return False
+
