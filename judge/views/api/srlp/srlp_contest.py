c10469071915f95dd03634617d434f1eb734bcc1
PAraya98 <pedroaraya3098@gmail.com>
2022-11-18 20:55:22 -0300

    bug fix

--- judge/views/api/srlp/srlp_contest.py
+++ judge/views/api/srlp/srlp_contest.py
@@ -397,7 +397,7 @@ def get_time(request):
                 has_limit_time = contest.time_limit and (contest.time_limit + timezone.now()) > contest.locked_after
                 if has_limit_time:
                     aux_date = contest.time_limit + timezone.now()
-                    conditional_time = aux_date.timestapm()
+                    conditional_time = aux_date.timestamp()
                 else:
                     conditional_time = contest.locked_after.timestamp() + 0.5
             else:
