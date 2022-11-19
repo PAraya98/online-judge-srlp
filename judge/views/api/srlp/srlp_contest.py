10891fb28d3887b628465374ec93af0bc2c81d2c
PAraya98 <pedroaraya3098@gmail.com>
2022-11-18 21:44:45 -0300

    bug fix

--- judge/views/api/srlp/srlp_contest.py
+++ judge/views/api/srlp/srlp_contest.py
@@ -398,8 +398,9 @@ def get_time(request):
             ### Time condition
             has_locked_time = contest.locked_after and contest.locked_after < contest.end_time
             if has_locked_time:
-                has_limit_time = contest.time_limit and (contest.time_limit + timezone.now()) > contest.locked_after and \
-                                 request.profile.current_contest.contest == contest
+                has_limit_time = contest.time_limit and request.profile and request.profile.current_contest and request.profile.current_contest.contest.key == contest.key and \
+                     (contest.time_limit + request.profile.current_contest.real_start) > contest.locked_after 
+                                 
                 if has_limit_time:
                     aux_date = contest.time_limit + request.profile.current_contest.real_start
                     conditional_time = aux_date.timestamp()
@@ -408,8 +409,8 @@ def get_time(request):
                     conditional_time = contest.locked_after.timestamp() + 0.5
                     message = 'El concurso se bloquea en: '+datetime.fromtimestamp(conditional_time - timezone_now).strftime("%d:%H:%M:%S")  
             else:
-                has_limit_time = contest.time_limit and (contest.time_limit + timezone.now()) > contest.end_time and \
-                                 request.profile.current_contest.contest == contest
+                has_limit_time = contest.time_limit and request.profile and request.profile.current_contest and request.profile.current_contest.contest.key == contest.key and \
+                     (contest.time_limit + request.profile.current_contest.real_start) > contest.end_time 
                 if has_limit_time:
                     aux_date = contest.time_limit + request.profile.current_contest.real_start
                     conditional_time = aux_date.timestamp()
