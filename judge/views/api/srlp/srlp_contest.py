c6619644754955d8be3d81a6a127d9843dfb2434
PAraya98 <pedroaraya3098@gmail.com>
2022-11-20 20:09:39 -0300

    bug fuxes

--- judge/views/api/srlp/srlp_contest.py
+++ judge/views/api/srlp/srlp_contest.py
@@ -91,7 +91,7 @@ def get_contest_info(request):
     contest_code = '' if not code else code[0]
     contest = Contest.objects.filter(key=contest_code).first()
 
-    if not contest and (contest.is_accessible_by(user)):
+    if not (contest and contest.is_accessible_by(user)):
        return Response({'status': False, 'message': 'El concurso no existe o no tienes acceso a este concurso.'})
 
     in_contest = contest.is_in_contest_rest(user)
